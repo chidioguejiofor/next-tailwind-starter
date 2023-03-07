69c4fc867b85a62ecd2392d1fd8c858f77d6ce04
Chidiebere Ogujeiofor <chidioguejiofor@gmail.com>
2023-03-07 16:01:44 +0100

    chore: setup prettier and commit msg check

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 
 type HeadingProps = {
   children: string;
-  type?: "h1" | "h2" | "h3";
+  type?: 'h1' | 'h2' | 'h3';
 };
+
 const Heading = (props: HeadingProps) => {
-  const Component = props.type || "h1";
+  const Component = props.type || 'h1';
   return <Component>{props.children}</Component>;
 };
 
